2c50013040b4e683ebd60a42a9aa82254a0a0ab1
Eric Crudup <briefcasemanx@gmail.com>
2022-02-21 15:12:54 -0800

    Project start. Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

---
+++ Models/SphinxRiddles.cs
@@ -0,0 +1,16 @@
+using System;
+
+
+
+public class Sphinx
+{
+  private string[] myRiddles = { "What has to be broken before you can use it?", "I’m the rare case when today comes before yesterday. What am I?", "What goes all the way around the world but stays in a corner?" }; 
+
+  private string[] myAnswers = { "Egg", "Dictionary", "Stamp" };
+
+  public string getRandomRiddle(){
+    
+  }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,11 @@
+using System;
+using Sphinx;
+
+
+class Program
+{
+  static void Main()
+  {
+    
+  }
+}
