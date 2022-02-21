1dfe8e76280c156192a351d4db804cca90edccd1
Eric Crudup <briefcasemanx@gmail.com>
2022-02-21 15:49:20 -0800

    random riddle generator working.
    Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

--- Models/SphinxRiddles.cs
+++ Models/SphinxRiddles.cs
@@ -1,16 +1,27 @@
 using System;
 
 
+namespace SphinxRiddle.Models{
+  public class Sphinx
+  {
+    private static string[] myRiddles = { "What has to be broken before you can use it?", "I’m the rare case when today comes before yesterday. What am I?", "What goes all the way around the world but stays in a corner?" }; 
 
-public class Sphinx
-{
-  private string[] myRiddles = { "What has to be broken before you can use it?", "I’m the rare case when today comes before yesterday. What am I?", "What goes all the way around the world but stays in a corner?" }; 
+    private static string[] myAnswers = { "Egg", "Dictionary", "Stamp" };
 
-  private string[] myAnswers = { "Egg", "Dictionary", "Stamp" };
+    public static string GetRandomRiddle(int randNum){
+      return myRiddles[randNum];
+    }
 
-  public string getRandomRiddle(){
-    
+    public static string CheckRiddleAnswer(int randNum, string response)
+    {
+      if (response == myAnswers[randNum]) {
+        return "YOU WIN!";
+      } else {
+        return "You've been eaten by the Sphinx!";
+      }
+    }
   }
 }
+
 
 
--- Program.cs
+++ Program.cs
@@ -1,11 +1,18 @@
 using System;
-using Sphinx;
+using System.Collections.Generic;
+using SphinxRiddle.Models;
 
 
-class Program
-{
-  static void Main()
+namespace SphinxRiddle {
+  class Program
   {
-    
+    static void Main()
+    {
+      Random rnd =  new Random();
+      int randNum = rnd.Next(0, 2);
+      Console.WriteLine( Sphinx.GetRandomRiddle(randNum) );
+      string response = Console.ReadLine();
+      Console.WriteLine( Sphinx.CheckRiddleAnswer(randNum, response) );
+    }
   }
 }
